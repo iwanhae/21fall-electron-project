e0bf71429b0ae28e8f2234881b11146715f32606
leegwae <leegwae@gmail.com>
2021-10-04 01:27:00 +0900

    feat: Add theme

---
+++ src/components/theme/index.ts
@@ -0,0 +1,15 @@
+import Theme from '@components/theme/type';
+
+export const theme: Theme = {
+  background: {
+    main: 'white',
+  },
+  color: {
+    primary: '#F9F3DF',
+    secondary: 'black',
+  },
+  border: {
+    radius: '4px',
+  },
+  boxShadow: '',
+};
---
+++ src/components/theme/type.ts
@@ -0,0 +1,13 @@
+export default interface Theme {
+  background: {
+    main: string;
+  };
+  color: {
+    primary: string;
+    secondary: string;
+  };
+  border: {
+    radius: string;
+  };
+  boxShadow: string;
+}
